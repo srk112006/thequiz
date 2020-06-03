bf97d187f46ae6016f331ec36d899ee8dc20968c
srk112006 <65439988+srk112006@users.noreply.github.com>
2020-06-03 12:07:56 +0530

    Add files via upload

---
+++ eco2.js
@@ -0,0 +1,26 @@
+
+var result = "";
+
+function validateQ1(){
+    
+    
+    var answer1 = document.getElementById('1A');
+
+    
+    //Checked is a special variable for radio buttons which is set to true when selected 
+    //and false when not selected
+    //If 1st choice is selected then set result to correct answer string
+    if(answer1.checked == true){
+        result = "AMAZING! Only one question left!";
+    }
+    else{
+        result = "Sorry! This answer is wrong! The right answer was A-white collar union";
+    }
+    //p tag (as in paragraph) in the end of the html has id result.
+    //innerHTML is used to change what the paragraph displays
+    //What it displays depends on the answer selected above
+    document.getElementById('result').innerHTML = result;
+
+    
+    
+}
